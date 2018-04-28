df29e58f24bc8ef7d66e80c3ded90325a6ee73ef
bingo8670 <bingo891111@gmail.com>
2018-04-28 16:33:42 +0800

    chapter 7 散列

---
+++ 008.votes.rb
@@ -0,0 +1,19 @@
+# 选票计数，hash
+
+lines = []
+File.open("votes.txt") do |file|
+  lines = file.readlines
+end
+
+votes = Hash.new(0)
+
+lines.each do |line|
+  name = line.chomp
+  name.upcase!
+  votes[name] += 1
+end
+
+votes.each do |name, count|
+  puts "#{name} : #{count}"
+end
+
